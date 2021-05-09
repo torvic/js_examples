e4277715c59b8326aa31aec3a54ce5b4872b0084
torvic <130111@unsaac.edu.pe>
2021-05-09 12:55:00 -0500

    crud fetch

--- javascript-apps/js/crud_heros.js
+++ javascript-apps/js/crud_heros.js
@@ -7,10 +7,12 @@ $fragment = d.createDocumentFragment();
 
 const getAll = async () => {
     try {
-        let res = await fetch("http://127.0.0.1:8000/hers/"),
+        let res = await fetch("http://127.0.0.1:8000/hero/"),
         json = await res.json();
 
+        console.log(res);
         if (!res.ok) throw {status: res.status, statusText: res.statusText};
+        console.log(json);
     } catch (err) {
         let message = err.statusText || "Ocurrio un error";
         $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
